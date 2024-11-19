bab51e8eddb8ef125f9bc6bd905ca94c2794993c
ObNitram <mart1.guillemot@gmail.com>
2024-11-19 12:32:33 +0100

    tme7 : Semaphore, Memoire partagee

--- TME7/src/Stack.h
+++ TME7/src/Stack.h
@@ -1,28 +1,63 @@
 #pragma once
 
-#include <cstring> // size_t,memset
+#include <cstring>     // size_t, memset
+#include <semaphore.h> // sem_t
 
-namespace pr {
+namespace pr
+{
 
 #define STACKSIZE 100
 
-template<typename T>
-class Stack {
-	T tab [STACKSIZE];
-	size_t sz;
-public :
-	Stack () : sz(0) { memset(tab,0,sizeof tab) ;}
+template <typename T> class Stack
+{
+    T tab[STACKSIZE];
+    size_t sz;
+    sem_t sem_empty{}; // Pour contrôler les éléments disponibles
+    sem_t sem_full{};  // Pour contrôler l'espace disponible
 
-	T pop () {
-		// bloquer si vide
-		T toret = tab[--sz];
-		return toret;
-	}
+  public:
+    // Constructeur
+    Stack() : sz(0)
+    {
+        memset(tab, 0, sizeof(tab));
+        // Initialiser les sémaphores
+        sem_init(&sem_empty, 1, 0);        // Commence à 0 (aucun élément disponible)
+        sem_init(&sem_full, 1, STACKSIZE); // Commence à STACKSIZE (plein d'espace libre)
+    }
 
-	void push(T elt) {
-		//bloquer si plein
-		tab[sz++] = elt;
-	}
+    // Destructeur
+    ~Stack()
+    {
+        // Détruire les sémaphores
+        sem_destroy(&sem_empty);
+        sem_destroy(&sem_full);
+    }
+
+    T pop()
+    {
+        // Bloquer si la pile est vide
+        sem_wait(&sem_empty);
+
+        // Accéder en toute sécurité à la pile
+        T toret = tab[--sz];
+
+        // Signaler qu'un espace est disponible
+        sem_post(&sem_full);
+
+        return toret;
+    }
+
+    void push(T elt)
+    {
+        // Bloquer si la pile est pleine
+        sem_wait(&sem_full);
+
+        // Accéder en toute sécurité à la pile
+        tab[sz++] = elt;
+
+        // Signaler qu'un élément est disponible
+        sem_post(&sem_empty);
+    }
 };
 
-}
+} // namespace pr
--- TME7/src/prod_cons.cpp
+++ TME7/src/prod_cons.cpp
@@ -1,51 +1,102 @@
 #include "Stack.h"
+#include <cstring>
+#include <fcntl.h>
 #include <iostream>
-#include <sys/wait.h>
+#include <sys/mman.h>
 #include <unistd.h>
-#include <vector>
 
-using namespace std;
-using namespace pr;
-
-void producteur(Stack<char> *stack)
+void producteur(pr::Stack<char> *stack)
 {
     char c;
-    while (cin.get(c))
+    while (std::cin.get(c))
     {
         stack->push(c);
     }
 }
 
-void consomateur(Stack<char> *stack)
+void consomateur(pr::Stack<char> *stack)
 {
-    while (true)
+    for (;;)
     {
-        char c = stack->pop();
-        cout << c << flush;
+        const char c = stack->pop();
+        std::cout << c << std::flush;
     }
 }
 
-int main()
+void print_help(const char *const argv[]) { std::cerr << "Usage: " << argv[0] << " producer|consumer" << std::endl; }
+
+int main(const int argc, char *argv[])
 {
-    auto s = new Stack<char>();
-
-    pid_t pp = fork();
-    if (pp == 0)
+    if (argc != 2)
     {
-        producteur(s);
-        return 0;
+        print_help(argv);
+        return 1;
     }
 
-    pid_t pc = fork();
-    if (pc == 0)
+    if (std::strcmp(argv[1], "producer") != 0 && std::strcmp(argv[1], "consumer") != 0)
     {
-        consomateur(s);
-        return 0;
+        print_help(argv);
+        return 1;
     }
 
-    wait(nullptr);
-    wait(nullptr);
+    const bool is_producer = (std::strcmp(argv[1], "producer") == 0);
 
-    delete s;
+    constexpr size_t size_of_stack = sizeof(pr::Stack<char>);
+    const auto shm_name = "/stack_shm";
+
+    const int shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1)
+    {
+        perror("shm_open");
+        return 1;
+    }
+
+    if (ftruncate(shm_fd, size_of_stack) == -1)
+    {
+        perror("ftruncate");
+        return 1;
+    }
+
+    void *ptr = mmap(nullptr, size_of_stack, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (ptr == MAP_FAILED)
+    {
+        perror("mmap");
+        return 1;
+    }
+
+    pr::Stack<char> *stack = nullptr;
+
+    if (is_producer)
+    {
+        // Init the stack
+        stack = new (ptr) pr::Stack<char>;
+    }
+    else // is consumer
+    {
+        // Get the stack
+        stack = static_cast<pr::Stack<char> *>(ptr);
+    }
+
+    if (is_producer)
+    {
+        producteur(stack);
+    }
+    else
+    {
+        consomateur(stack);
+    }
+
+    // Only the producer clean
+    if (is_producer)
+    {
+        stack->~Stack();
+        munmap(ptr, size_of_stack);
+        shm_unlink(shm_name);
+    }
+    else
+    {
+        munmap(ptr, size_of_stack);
+    }
+
     return 0;
 }
